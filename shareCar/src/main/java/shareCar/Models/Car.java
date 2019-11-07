74c3ba645591a2e8c65ca1594bd951c7eb5f1f45
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-11-07 19:31:27 +0100

    add car model, getters, setters

---
+++ shareCar/src/main/java/shareCar/Models/Car.java
@@ -0,0 +1,84 @@
+package shareCar.Models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Car {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column
+  private Long id;
+
+  @Column
+  private String registrationNumber;
+
+  @Column
+  private String type;
+
+  @Column
+  private Integer numberOfSeats;
+
+  @Column
+  private Integer ConsumptionPerKm;
+
+  @ManyToOne
+  @JoinColumn(name = "owner_id", nullable = false)
+  private Person owner;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getRegistrationNumber() {
+    return registrationNumber;
+  }
+
+  public void setRegistrationNumber(String registrationNumber) {
+    this.registrationNumber = registrationNumber;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public Integer getNumberOfSeats() {
+    return numberOfSeats;
+  }
+
+  public void setNumberOfSeats(Integer numberOfSeats) {
+    this.numberOfSeats = numberOfSeats;
+  }
+
+  public Integer getConsumptionPerKm() {
+    return ConsumptionPerKm;
+  }
+
+  public void setConsumptionPerKm(Integer consumptionPerKm) {
+    ConsumptionPerKm = consumptionPerKm;
+  }
+
+  public Person getOwner() {
+    return owner;
+  }
+
+  public void setOwner(Person owner) {
+    this.owner = owner;
+  }
+}
--- shareCar/src/main/java/shareCar/Models/Person.java
+++ shareCar/src/main/java/shareCar/Models/Person.java
@@ -1,11 +1,13 @@
 package shareCar.Models;
 
 import java.time.LocalDate;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -29,4 +31,46 @@ public class Person {
   @Column
   private LocalDate dateOfBirth;
 
+  @OneToMany(mappedBy = "owner")
+  private List<Car> cars;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public Gender getGender() {
+    return gender;
+  }
+
+  public void setGender(Gender gender) {
+    this.gender = gender;
+  }
+
+  public LocalDate getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public void setDateOfBirth(LocalDate dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
 }
