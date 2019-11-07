81e64dcacc996dd0f9f7d0746d273e611633fdde
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-11-07 19:02:03 +0100

    initialize project

---
+++ shareCar/src/main/java/shareCar/ShareCarApplication.java
@@ -0,0 +1,13 @@
+package shareCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareCarApplication.class, args);
+	}
+
+}
---
+++ shareCar/src/test/java/shareCar/ShareCarApplicationTests.java
@@ -0,0 +1,13 @@
+package shareCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
